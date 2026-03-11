ce76ff2089f179c098d1e68258eadfe70e9aab68
Henry3244 <henrywhats64@gmail.com>
2026-03-11 16:52:38 -0400

    Update lspconfig.lua

--- nvim/lua/configs/lspconfig.lua
+++ nvim/lua/configs/lspconfig.lua
@@ -7,7 +7,7 @@ local lspconfig = require "lspconfig"
 local servers = { "html", "cssls", "ts_ls", "tailwindcss" }
 local nvlsp = require "nvchad.configs.lspconfig"
 
--- lsps with default config (all)
+-- lsps with default config (manual)
 for _, lsp in ipairs(servers) do
   lspconfig[lsp].setup {
     on_attach = nvlsp.on_attach,
@@ -18,7 +18,7 @@ end
 
 -- configuring single server (custom) - typescript
  lspconfig.ts_ls.setup {
-   on_attach = nvlsp.on_attach,
-   on_init = nvlsp.on_init,
+   on_attach = nvlsp.attach,
+   on_init = nvlsp.init,
    capabilities = nvlsp.capabilities,
  }
